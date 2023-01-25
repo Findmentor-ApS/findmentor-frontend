77b2a3d68d091a7e8c91a8cbc69b4bde0d9d39cf
Ahmad Almajedi <kontakt@ahmad.nu>
2023-01-25 03:19:10 +0100

    wip

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -6,20 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  signInCondition: boolean = false;
-  logOutCondition: boolean = true;
-
-
   constructor() { }
-  signIn() {
-    this.signInCondition = !this.signInCondition;
-    this.logOutCondition = !this.logOutCondition;
-  }
-  logOut() {
-    this.logOutCondition = !this.logOutCondition;
-    this.signInCondition = !this.signInCondition;
-  }
-  ngOnInit(): void {
 
-  }
+  ngOnInit(): void { }
 }
