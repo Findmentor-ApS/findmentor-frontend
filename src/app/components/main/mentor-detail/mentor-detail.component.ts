7532d6acd5d3c94a7d67d4644cb282f4484190b5
samilesma <samilesma@gmail.com>
2023-04-03 13:15:09 +0200

    Mentor detail

--- src/app/components/main/mentor-detail/mentor-detail.component.ts
+++ src/app/components/main/mentor-detail/mentor-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { experienceType } from 'src/app/general/types';
 
 @Component({
   selector: 'app-mentor-detail',
@@ -9,12 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 export class MentorDetailComponent implements OnInit {
 
   mentor: any;
+  experienceTypeMap: {[key: number]: string} = {};
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    experienceType.forEach(type => {
+      this.experienceTypeMap[type.id] = type.name;
+    });
+
     this.route.data.subscribe((data: { mentor: any }) => {
       this.mentor = data.mentor;
+      this.mentor.experiences = data.mentor.experiences.map((exp) => parseInt(exp.experience_type));
     });
     console.log(this.mentor);
   }
