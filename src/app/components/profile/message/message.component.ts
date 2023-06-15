859e93a060010ab5ea81a6753d2f1d5ca500626d
samilesma <samilesma@gmail.com>
2023-06-15 11:38:33 +0200

    CSS rettelser

--- src/app/components/profile/message/message.component.ts
+++ src/app/components/profile/message/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MessagingService } from 'src/app/services/messaging.service';
 import { UserDataService } from 'src/app/services/user-data.service';
 
@@ -9,18 +9,18 @@ import { UserDataService } from 'src/app/services/user-data.service';
 })
 export class MessageComponent implements OnInit {
 
-  // MessageComponent
-
+  @ViewChild('messagesContainer', { static: false }) messagesContainer: ElementRef;
   public messages: any = [];
   public contacts: any = [];
   public messageText: string = '';
   public selectedContact: any = null;
   page: number = 0;
   loading: boolean = false;
+  isSending: boolean = false;
   user: any = null;
   currentChannel: any;
 
-  constructor(private messagingService: MessagingService, private userDataService: UserDataService) { }
+  constructor(private messagingService: MessagingService, private userDataService: UserDataService,@Inject('ASSET_PATH') public assetPath: string) { }
 
   ngOnInit(): void {
       this.messagingService.getContacts().subscribe((contacts: any) => {
@@ -51,6 +51,7 @@ export class MessageComponent implements OnInit {
 
       this.messagingService.getMessagesForContact(contact.contact_id, contact.contact_type).subscribe((messages: any) => {
         this.messages = messages.reverse();
+        this.scrollToBottom();
       });
   }
 
@@ -70,27 +71,39 @@ export class MessageComponent implements OnInit {
 
 
   sendMessage(): void {
-      const userData = {
-          content: this.messageText,
-          receiver_id: this.selectedContact.contact_id,
-          receiver_type: this.selectedContact.contact_type,
-          sender_id: this.user.id,
-          sender_type: localStorage.getItem('type'),
-          created_at: new Date()
+    if (this.isSending) {
+        return;
       }
+    const userData = {
+        content: this.messageText,
+        receiver_id: this.selectedContact.contact_id,
+        receiver_type: this.selectedContact.contact_type,
+        sender_id: this.user.id,
+        sender_type: localStorage.getItem('type'),
+        created_at: new Date()
+    }
 
-      if (this.messageText.trim() !== '') {
-          this.messagingService.sendMessage(userData).subscribe(() => {
+    if (this.messageText.trim() !== '') {
+        this.isSending = true;
+        this.messagingService.sendMessage(userData).subscribe(() => {
 
-              // const contact = this.contacts.find(c => c.contact_id === this.selectedContact.contact_id && c.contact_type === this.selectedContact.contact_type);
-              // if (contact) {
-              //     contact.last_message_content = this.messageText;
-              // }
+            // const contact = this.contacts.find(c => c.contact_id === this.selectedContact.contact_id && c.contact_type === this.selectedContact.contact_type);
+            // if (contact) {
+            //     contact.last_message_content = this.messageText;
+            // }
 
-              this.messageText = '';
+            this.isSending = false;
+            this.messageText = '';
+            this.scrollToBottom();
 
-          });
-      }
+        });
+    }
+  }
+
+  scrollToBottom(): void {
+    setTimeout(() => {
+      this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+    });
   }
 
 }
