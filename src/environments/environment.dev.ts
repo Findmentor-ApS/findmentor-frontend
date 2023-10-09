689f6e796637ecf7b5e007f722a09b6df1a175c6
Ahmad Almajedi <kontakt@ahmad.nu>
2023-10-09 20:51:49 +0200

    Fix frontend env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://findmentor.dk/api"
+  apiUrl: "https://dev.findmentor.dk/api"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://dev.findmentor.dk/api"
+  apiUrl: "http://localhost:8080/api"
 };
 
 /*
